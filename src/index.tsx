7f019b30710a282c326baf904eeba66e8e4ae408
WillN-Git <wfndefo@gmail.com>
2022-08-20 21:26:12 +0200

    🐣Init project

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    line-height: 1;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html, body {
+    cursor: none;
+    background-color: #e1dfdd;
+  }
+
+  * {
+    min-height: 0;
+    min-width: 0;
+    box-sizing: border-box;
+    -webkit-backface-visibility: hidden;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+`;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    // prettier-ignore
+    document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(  
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
